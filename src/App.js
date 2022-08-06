9846546b60a9a77c3cb0d2638cc53debd8a469d4
paulooliveira152012 <paulooliveira152012@gmail.com>
2022-08-05 21:34:28 -0400

    why background image not displaying?

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import About from './components/about';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="my-5">
+  <h1 id="about">Who am I?</h1>
+</section>
+    
   );
 }
 
